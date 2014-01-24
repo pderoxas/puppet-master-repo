eebed0d683be0881853e51a9f2f67b86ebbe61c7
Paolo de Roxas <paolo.deroxas@gmail.com>
2014-01-24 17:20:26 -0500

    tokenize the java config

--- modules/sdk/manifests/java.pp
+++ modules/sdk/manifests/java.pp
@@ -1,6 +1,13 @@
 class sdk::java {
   #any java specific implementation can go here...
 
+  $openIdService.baseURI = hiera('openIdService.baseURI')
+  $openIdService.refreshToken = hiera('openIdService.refreshToken')
+  $openIdService.clientID = hiera('openIdService.clientID')
+  $openIdService.clientSecret = hiera('openIdService.clientSecret')
+  $locationService.baseURI = hiera('locationService.baseURI')
+  $location.id = hiera('location.id')
+
   file { "SystemClient.properties":
         path    => "$sdk::sdk_root_dir/sdk/SystemClient.properties",
         owner   => 'root',

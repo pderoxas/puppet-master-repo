13bdc8e9402207790aefd68820efe99b2a1e3369
Paolo de Roxas <paolo.deroxas@gmail.com>
2014-01-24 17:24:31 -0500

    removed periods from variable names in manifest

--- modules/sdk/manifests/java.pp
+++ modules/sdk/manifests/java.pp
@@ -1,12 +1,12 @@
 class sdk::java {
   #any java specific implementation can go here...
 
-  $openIdService.baseURI = hiera('openIdService.baseURI')
-  $openIdService.refreshToken = hiera('openIdService.refreshToken')
-  $openIdService.clientID = hiera('openIdService.clientID')
-  $openIdService.clientSecret = hiera('openIdService.clientSecret')
-  $locationService.baseURI = hiera('locationService.baseURI')
-  $location.id = hiera('location.id')
+  $openIdServiceBaseURI = hiera('openIdService.baseURI')
+  $openIdServiceRefreshToken = hiera('openIdService.refreshToken')
+  $openIdServiceClientID = hiera('openIdService.clientID')
+  $openIdServiceClientSecret = hiera('openIdService.clientSecret')
+  $locationServiceBaseURI = hiera('locationService.baseURI')
+  $locationId = hiera('location.id')
 
   file { "SystemClient.properties":
         path    => "$sdk::sdk_root_dir/sdk/SystemClient.properties",

2b27adbf81293fcb3c4c0c64c297947bc06dce00
Paolo de Roxas <paolo.deroxas@gmail.com>
2014-01-24 18:58:52 -0500

    Using scoped variable names

--- modules/sdk/manifests/dotnet.pp
+++ modules/sdk/manifests/dotnet.pp
@@ -1,6 +1,20 @@
 class sdk::dotnet {
   #any .NET specific implementation can go here...
 
+   $openIdServiceBaseURI = hiera('openIdService.baseURI', "")
+   $hereAPIServiceBaseURI = hiera('hereAPIService.baseURI', "")
+   $posAPIServiceBaseURI = hiera('posAPIService.baseURI', "")
+   $ecAPIServiceBaseURI = hiera('ecAPIService.baseURI', "")
+   $payerId = hiera('payerId', "")
+   $locationId = hiera('locationId', "")
+   $userId = hiera('userId', "")
+   $password = hiera('password', "")
+   $signature = hiera('signature', "")
+   $password = hiera('password', "")
+   $refreshToken = hiera('refreshToken', "")
+   $clientSecret = hiera('clientSecret', "")
+
+
   file { "Config.xml":
         path    => "$sdk::sdk_root_dir/sdk/Config.xml",
         owner   => 'root',
